a97b0551960661d4a082fea7b3748a169dc5d26c
Rory Standley <rorystandley@gmail.com>
2020-03-04 16:19:53 +0000

    Unit testing article controller

--- src/article/article.controller.spec.ts
+++ src/article/article.controller.spec.ts
@@ -1,18 +1,70 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { ArticleController } from './article.controller';
+import { ArticleService } from "./article.service";
+import { Article } from "./article.interface";
+import { getModelToken } from '@nestjs/mongoose';
 
-describe('News Controller', () => {
-  let controller: ArticleController;
+describe( 'Article Controller', () => {
+	let controller: ArticleController;
+	let articleService: ArticleService;
 
-  beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      controllers: [ArticleController],
-    }).compile();
+	class EventModel {
+		constructor( private data ) {
+		}
 
-    controller = module.get<ArticleController>(ArticleController);
-  });
+		create = jest.fn().mockResolvedValue( this.data );
+		static findByArticleId = jest.fn().mockResolvedValue( {} );
+		static findByTeam = jest.fn().mockResolvedValue( {} );
+	}
 
-  it('should be defined', () => {
-    expect(controller).toBeDefined();
-  });
-});
+	beforeEach( async () => {
+		const module: TestingModule = await Test.createTestingModule( {
+			controllers: [ArticleController],
+			providers: [
+				ArticleService,
+				{ provide: getModelToken( 'Article' ), useClass: EventModel },
+			]
+		} ).compile();
+
+		controller = module.get<ArticleController>( ArticleController );
+		articleService = module.get<ArticleService>( ArticleService );
+	} );
+
+	describe( 'findByTeam', () => {
+		it( 'should return an array of articles', async () => {
+			const result = [
+				{
+					id: '123',
+					title: 'Test Title',
+					description: 'Test escription',
+					source: 'Test source',
+					url: 'https://google.com',
+					image: 'https://google.com/image.jpg'
+				}
+			];
+			// @ts-ignore
+			jest.spyOn( articleService, 'findByTeam' ).mockImplementation( () => result );
+
+			expect( await controller.findByTeam( 'team' ) ).toBe( result );
+		} );
+	} );
+
+	describe( 'findByArticleId', () => {
+		it( 'should return an article', async () => {
+			const result =
+				{
+					id: '123',
+					title: 'Test Title',
+					description: 'Test escription',
+					source: 'Test source',
+					url: 'https://google.com',
+					image: 'https://google.com/image.jpg'
+				};
+			// @ts-ignore
+			jest.spyOn( articleService, 'findByArticleId' ).mockImplementation( () => result );
+
+			expect( await controller.findById( 'celtic', '123' ) ).toBe( result );
+		} );
+	} );
+
+} );
